0538f06946a059aea7d478ec5323c2a6d151a464
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-11-13 14:12:49 +0530

    changes UI
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:social_media_app_starter/screens/onboarding_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterShare',
       debugShowCheckedModeBanner: false,
-      home: Text("Hello World"),
+      theme: ThemeData(
+        textTheme: GoogleFonts.interTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: OnboardingScreen(),
     );
   }
 }
--- lib/widgets/progress.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-circularProgress() {
-  return Text("circular progress");
-}
-
-linearProgress() {
-  return Text("linear progress");
-}
